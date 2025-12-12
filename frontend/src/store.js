db64aabad9c43cd49a248eec51999e0d9fb87513
flameztech01 <flameztech01@gmail.com>
2025-12-12 20:05:49 +0100

    auth

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authslice.js";
+import authReducer from "./slices/authSlice.js";
 import { apiSlice } from "./slices/apiSlice.js";
 
 const store = configureStore({
