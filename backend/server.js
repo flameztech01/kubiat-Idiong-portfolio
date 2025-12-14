35e76f32802f4794164a4d7e9fefaceeb8c84f90
flameztech01 <flameztech01@gmail.com>
2025-12-14 15:33:24 +0100

    mema

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,7 @@ app.use(cors({
         'http://localhost:3000',
         'https://njoku-samuel-portfolio.onrender.com',
         'https://www.flameztech.online',
+        'https://flameztech.online'
     ],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
